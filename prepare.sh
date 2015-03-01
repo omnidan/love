248355c53094d71fbada27c02e29a3486dda40f3
Daniel Bugl <daniel.bugl@touchlay.com>
2015-03-01 19:40:37 +0100

    add plugin installation to prepare script

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mv platforms/android/ platforms/android.bak/
 cordova platform add android
+cordova plugin add https://github.com/phonegap-build/PushPlugin.git
 cp -R platforms/android.bak/* platforms/android/
 rm -rf platforms/android.bak/
 echo "Success! Now run './build.sh'"
